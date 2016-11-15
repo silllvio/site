4f14faf8f8c2a0ea9293d14603a500b026a8c0c8
silllvio <silllvio@hotmail.com>
2016-11-15 20:39:52 -0200

    limpeza

--- redesocial/redesocial/perfis/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Perfil():
-    def __init__(self, nome='', email='', telefone ='', empresa = ''):
-        self.nome = nome
-        self.email = email
-        self.telefone = telefone
-        self.empresa  = empresa
-
